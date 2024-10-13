a8c3db5a078d22a3d0408e3ca72d3098a9cfdbe3
Rachith Prakash <rachithprakash@gmail.com>
2024-10-13 12:02:32 -0700

    add sd2_fp16_ov results, update code

--- run_scoring.py
+++ run_scoring.py
@@ -1,16 +1,19 @@
 import argparse
 import json
+import os
 from tifascore import tifa_score_benchmark
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--imgs_file", required=True)
+parser.add_argument("--output_file", required=True)
 args = parser.parse_args()
 
+assert os.path.exists(args.imgs_file)
 
 # generate json 
 results = tifa_score_benchmark("mplug-large", "D:/github/tifa/tifa_v1.0/tifa_v1.0_question_answers.json", args.imgs_file)
 
 # save the results
-with open("D:/github/tifa/image_generation/sd2/results.json", "w") as f:
+with open(args.output_file, "w") as f:
     json.dump(results, f, indent=4)

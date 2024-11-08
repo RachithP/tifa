7c09b43bf8e6a728266a1b2cf4df7839dea2dec2
rachithp <rachithprakash@gmail.com>
2024-11-08 14:32:46 -0800

    remove element_type

--- run_scoring.py
+++ run_scoring.py
@@ -7,12 +7,11 @@ from tifascore import tifa_score_benchmark
 parser = argparse.ArgumentParser()
 parser.add_argument("--imgs_file", required=True)
 parser.add_argument("--output_file", required=True)
-parser.add_argument("--element_type")
 args = parser.parse_args()
 
 assert os.path.exists(args.imgs_file)
 
-results = tifa_score_benchmark("mplug-large", "/home/rachithp/code/tifa/tifa_v1.0/tifa_v1.0_question_answers.json", args.imgs_file, args.element_type)
+results = tifa_score_benchmark("mplug-large", "/home/rachithp/code/tifa/tifa_v1.0/tifa_v1.0_question_answers.json", args.imgs_file)
 
 # save the results
 with open(args.output_file, "w") as f:
